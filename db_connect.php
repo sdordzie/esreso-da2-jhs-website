6761bd0c74d093b3a82815dd8e0a5b459f684099
sdordzie <123095782+sdordzie@users.noreply.github.com>
2025-04-14 18:57:35 +0000

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,13 +1,14 @@
 <?php
+// db_connect.php
 $host = 'localhost';
 $dbname = 'esreso_portal';
-$username = 'root';
-$password = '';
+$username = 'root'; // Change as needed
+$password = ''; // Change as needed
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    die("Database connection failed: " . $e->getMessage());
 }
 ?>
